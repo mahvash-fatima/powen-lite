d5cebd809cdb20e4b312c26af3088027a891a13d
memash <mash2x2@gmail.com>
2016-08-25 14:27:51 +0530

    Add media query for back to top button

--- 404.php
+++ 404.php
@@ -7,10 +7,22 @@
 ?>
 
 <?php get_header();  ?>
-	<section id="error-404" class="not-found">
-		<header class="page-header">
-			<h2 class="page-title"><?php _e( 'oops! That page can&rsquo;t be found.', 'powen-lite' ); ?></h2>
-		</header><!-- .page-header -->
-		<?php do_action( 'powen_footer_top_extras' ); ?>
+
+	<section class="error-404 not-found">
+		<div class="powen-wrapper">
+			<header class="page-header">
+				<span class="fa fa-exclamation-circle"></span>
+				<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'powen-lite' ); ?></h1>
+			</header><!-- .page-header -->
+
+			<div class="page-content">
+				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'powen-lite' ); ?></p>
+
+				<?php get_search_form(); ?>
+			</div><!-- .page-content -->
+		</div>
 	</section><!-- .error-404 -->
+
+<?php do_action( 'powen_footer_top_extras' ); ?>
+
 <?php get_footer(); ?>
